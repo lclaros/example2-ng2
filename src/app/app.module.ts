98c3132d821f3c291261287174be69beaa43a63f
Luis Arce <luis.arce22.a@gmail.com>
2016-11-23 21:42:27 -0400

    Router implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { BodyComponentComponent } from './body-component/body-component.component';
 import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'contact', component: ContactComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +25,16 @@ import { FooterComponentComponent } from './footer-component/footer-component.co
     MenuBarComponent,
     HeaderComponentComponent,
     BodyComponentComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    HomeComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
